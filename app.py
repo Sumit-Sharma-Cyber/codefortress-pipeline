1e48da5248496e55d8e80b82835aac2053b6a1a9
dhananjayjoddha-cloud <dhananjayjoddha@gmail.com>
2026-01-02 10:30:23 +0530

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # --- WEEK 1 & 2 SECRETS (For Trufflehog/SonarQube) ---
+# The following AWS credentials are FAKE and FOR TESTING PURPOSES ONLY.
+# These keys DO NOT work and must never be used in production environments.
 AWS_ACCESS_KEY_ID = "AKIA123456789TESTKEY"
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYTESTKEY"
 
@@ -22,5 +24,6 @@ def home():
 if __name__ == '__main__':
     # Run on port 5000 for the Docker container
     app.run(host='0.0.0.0', port=5000)
+
 
 
